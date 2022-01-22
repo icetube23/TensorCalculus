580c13be0d37a2e8920915df0f98847de7a4c959
Elias Kempf <eli2008@gmx.de>
2022-01-22 21:09:10 +0100

    Added indexing support for Tensor type.

--- src/tensor.jl
+++ src/tensor.jl
@@ -1,7 +1,13 @@
+"""
+Awesome `Tensor` data type!
+"""
 struct Tensor{T, N}
     data::Array{T, N}
 end
 
+Base.getindex(t::Tensor, args...; kwargs...) = getindex(t.data, args...; kwargs...)
+Base.setindex!(t::Tensor, args...; kwargs...) = setindex!(t.data, args...; kwargs...)
+Base.axes(t::Tensor, args...; kwargs...) = axes(t.data, args...; kwargs...)
 Base.ndims(t::Tensor) = ndims(t.data)
 Base.size(t::Tensor) = size(t.data)
 Base.eltype(t::Tensor) = eltype(t.data)
