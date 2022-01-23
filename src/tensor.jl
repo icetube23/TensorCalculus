cd1a6d12b5377d60fb5398657724bdd2cd3199c8
Elias Kempf <34234056+icetube23@users.noreply.github.com>
2022-01-23 16:38:05 +0100

    Changed Tensor indexing to be more consistent and added more reasonable tests.

--- src/tensor.jl
+++ src/tensor.jl
@@ -1,13 +1,28 @@
+using ArgCheck
+
 """
 Awesome `Tensor` data type!
 """
-struct Tensor{T, N}
+struct Tensor{T<:Number, N}
     data::Array{T, N}
+    Tensor(arr::Array{T, N}) where {T, N} = new{T, N}(arr)
+    Tensor(val::T) where T = new{T, 0}(fill(val))
 end
 
-Base.getindex(t::Tensor, args...; kwargs...) = getindex(t.data, args...; kwargs...)
-Base.setindex!(t::Tensor, args...; kwargs...) = setindex!(t.data, args...; kwargs...)
-Base.axes(t::Tensor, args...; kwargs...) = axes(t.data, args...; kwargs...)
-Base.ndims(t::Tensor) = ndims(t.data)
+# Extend array property methods to tensors
 Base.size(t::Tensor) = size(t.data)
+Base.ndims(t::Tensor) = ndims(t.data)
 Base.eltype(t::Tensor) = eltype(t.data)
+
+# Allow indexing tensors like arrays
+function Base.getindex(t::Tensor, args...; kwargs...)
+    @argcheck ndims(t) > 0 BoundsError(t)
+    val = getindex(t.data, args...; kwargs...)
+    Tensor(typeof(val) <: AbstractArray ? val : fill(val))
+end
+
+Base.setindex!(t::Tensor, val::Tensor, args...; kwargs...) = setindex!(t.data, val.data, args...; kwargs...)
+Base.setindex!(t::Tensor, val::T, args...; kwargs...) where {T<:Number} = setindex!(t.data, val, args...; kwargs...)
+
+Base.axes(t::Tensor, args...; kwargs...) = axes(t.data, args...; kwargs...)
+Base.eachindex(t::Tensor, args...; kwargs...) = eachindex(t.data, args...; kwargs...)
--- test/tensor.jl
+++ test/tensor.jl
@@ -1,9 +1,48 @@
 using TensorCalculus
 using Test
 
-@testset "tensor.jl" begin
+@testset "Array-like properties" begin
     t1 = Tensor(rand(3, 3))
+    @test size(t1) == (3, 3)
     @test ndims(t1) == 2
-    t2 = Tensor(rand(3, 3, 3))
+    @test eltype(t1) == Float64
+
+    t2 = Tensor(rand(Int32, 3, 3, 3))
+    @test size(t2) == (3, 3, 3)
     @test ndims(t2) == 3
+    @test eltype(t2) == Int32
+
+    t3 = Tensor(3f0)
+    @test size(t3) == ()
+    @test ndims(t3) == 0
+    @test eltype(t3) == Float32
+end
+
+@testset "Indexing" begin
+    # TODO: update comparisons once tensor equality is implemented
+    t = Tensor(rand(1, 2, 3, 3))
+    @test axes(t) == (Base.OneTo(1), Base.OneTo(2), Base.OneTo(3), Base.OneTo(3))
+    @test eachindex(t) == Base.OneTo(18)
+
+    t1 = t[1, :, :, :]
+    @test t1 isa Tensor{Float64, 3}
+    @test size(t1) == (2, 3, 3)
+    @test t1.data == t.data[1, :, :, :]
+
+    t2 = t[:, 2, :, 3]
+    @test t2 isa Tensor{Float64, 2}
+    @test size(t2) == (1, 3)
+    @test t2.data == t.data[:, 2, :, 3]
+
+    t3 = t[1, 2, 3, 2]
+    @test t3 isa Tensor{Float64, 0}
+    @test size(t3) == ()
+    @test t3.data == fill(t.data[1, 2, 3, 2])
+    @test_throws BoundsError t3[1]
+
+    t[1, 1, 1, :] = Tensor([1, 2, 3])
+    @test t[1, 1, 1, :].data == [1., 2., 3.]
+
+    t[1, 1, 1, 2] = 4
+    @test t[1, 1, 1, :].data == [1., 4., 3.]
 end
