e48c59f36523a494961da6cfaa826a02c8ff879a
Elias Kempf <34234056+icetube23@users.noreply.github.com>
2022-01-23 23:16:46 +0100

    Implemented == operator for tensors and adjusted tests to use the new syntax.

--- src/tensor.jl
+++ src/tensor.jl
@@ -9,9 +9,14 @@ struct Tensor{T<:Number, N}
     Tensor(val::T) where T = new{T, 0}(fill(val))
 end
 
+# TODO: Add reshape and permutedims methods
+# TODO: Add show methods
+# TODO: Customize tensor broadcasting
+
 # Extend array property methods to tensors
 Base.size(t::Tensor) = size(t.data)
 Base.ndims(t::Tensor) = ndims(t.data)
+Base.length(t::Tensor) = length(t.data)
 Base.eltype(t::Tensor) = eltype(t.data)
 
 # Allow indexing tensors like arrays
@@ -26,3 +31,10 @@ Base.setindex!(t::Tensor, val::T, args...; kwargs...) where {T<:Number} = setind
 
 Base.axes(t::Tensor, args...; kwargs...) = axes(t.data, args...; kwargs...)
 Base.eachindex(t::Tensor, args...; kwargs...) = eachindex(t.data, args...; kwargs...)
+
+# Extent array equality to tensors
+Base.:(==)(t1::Tensor, t2::Tensor) = t1.data == t2.data
+
+# Base.show overload examples
+# Base.show(io::IO, ::MIME"text/plain", m::MyType) = print(io, "Examplary instance of MyType\n", m.x, " ± ", m.y)
+# Base.show(io::IO, m::MyType) = print(io, m.x, '(', m.y, ')')
--- test/tensor.jl
+++ test/tensor.jl
@@ -5,16 +5,19 @@ using Test
     t1 = Tensor(rand(3, 3))
     @test size(t1) == (3, 3)
     @test ndims(t1) == 2
+    @test length(t1) == 9
     @test eltype(t1) == Float64
 
     t2 = Tensor(rand(Int32, 3, 3, 3))
     @test size(t2) == (3, 3, 3)
     @test ndims(t2) == 3
+    @test length(t2) == 27
     @test eltype(t2) == Int32
 
     t3 = Tensor(3f0)
     @test size(t3) == ()
     @test ndims(t3) == 0
+    @test length(t3) == 1
     @test eltype(t3) == Float32
 end
 
@@ -27,22 +30,22 @@ end
     t1 = t[1, :, :, :]
     @test t1 isa Tensor{Float64, 3}
     @test size(t1) == (2, 3, 3)
-    @test t1.data == t.data[1, :, :, :]
+    @test t1 == t[1, :, :, :]
 
     t2 = t[:, 2, :, 3]
     @test t2 isa Tensor{Float64, 2}
     @test size(t2) == (1, 3)
-    @test t2.data == t.data[:, 2, :, 3]
+    @test t2 == t[:, 2, :, 3]
 
     t3 = t[1, 2, 3, 2]
     @test t3 isa Tensor{Float64, 0}
     @test size(t3) == ()
-    @test t3.data == fill(t.data[1, 2, 3, 2])
+    @test t3 == t[1, 2, 3, 2]
     @test_throws BoundsError t3[1]
 
     t[1, 1, 1, :] = Tensor([1, 2, 3])
-    @test t[1, 1, 1, :].data == [1., 2., 3.]
+    @test t[1, 1, 1, :] == Tensor([1., 2., 3.])
 
     t[1, 1, 1, 2] = 4
-    @test t[1, 1, 1, :].data == [1., 4., 3.]
+    @test t[1, 1, 1, :] == Tensor([1., 4., 3.])
 end
