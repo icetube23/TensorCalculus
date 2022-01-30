4f657f9c67ca68b93d24d4d0fa987e537dfe0fc3
Documenter.jl <documenter@juliadocs.github.io>
2022-01-30 11:40:49 +0000

    build based on a6fad51

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TensorCalculus.jl","page":"Home","title":"TensorCalculus.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TensorCalculus.jl","category":"page"},{"location":"","page":"Home","title":"Home","text":"Tensor","category":"page"},{"location":"#TensorCalculus.Tensor","page":"Home","title":"TensorCalculus.Tensor","text":"Awesome Tensor data type!\n\n\n\n\n\n","category":"type"}]
+}
